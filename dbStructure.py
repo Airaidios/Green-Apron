93f5f9bcf1524c13bc7140adfbe6571b583146bf
peter gray <airaidios@gmail.com>
2018-04-16 09:59:45 +0100

    database improvements
    Client Table - Removed Order ID
    Kit Table - Removed Rec ID
    Rec Table - Removed Ing ID

--- dbStructure.py
+++ dbStructure.py
@@ -12,65 +12,49 @@ def executeSQL(
 db_name = "ga.db"
 
 clientTable = """CREATE TABLE IF NOT EXISTS CLIENT
-    (
-    client_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    (client_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     username text,
     address text,
     package integer,
     diet_req text,
     email text,
     level integer,
-    password text
-    )"""
+    password text)"""
 executeSQL(db_name, clientTable)
 
 orderTable = """CREATE TABLE IF NOT EXISTS "ORDER"
-    (
-    order_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    (order_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     client_id integer,
     kit_id integer,
     foreign key(client_id) references CLIENT(client_id)on update cascade on delete cascade
-    foreign key(kit_id) references KIT(kit_id)on update cascade on delete cascade
-    )"""
+    foreign key(kit_id) references KIT(kit_id)on update cascade on delete cascade)"""
 executeSQL(db_name, orderTable)
 
 kitTable = """CREATE TABLE IF NOT EXISTS KIT
-    (
-    kit_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    (kit_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     kit_name text,
     price float,
-    size text,
-    order_id integer,
-    rec_id integer,
-    foreign key(order_id) references "ORDER"(order_id)on update cascade on delete cascade
-    foreign key(rec_id) references RECIPE(rec_id)on update cascade on delete cascade
-    )"""
+    size text)"""
 executeSQL(db_name, kitTable)
 
 recipeTable = """CREATE TABLE IF NOT EXISTS RECIPE
-    (
-    rec_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    (rec_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     rec_name text,
     culture text
     servings integer,
     prep_time integer,
     kit_id integer,
-    ing_id integer,
-    foreign key(kit_id) references KIT(kit_id)on update cascade on delete cascade
-    foreign key(ing_id) references INGREDIENT(ing_id)on update cascade on delete cascade
-    )"""
+    foreign key(kit_id) references KIT(kit_id)on update cascade on delete cascade)"""
 executeSQL(db_name, recipeTable)
 
 ingredientTable = """CREATE TABLE IF NOT EXISTS INGREDIENT
-    (
-    ing_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    (ing_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     ing_name text,
     ing_quant integer,
     allergy text,
     type text,
     rec_id integer,
-    foreign key(rec_id) references RECIPE(rec_id)on update cascade on delete cascade
-    )"""
+    foreign key(rec_id) references RECIPE(rec_id)on update cascade on delete cascade)"""
 executeSQL(db_name, ingredientTable)
 
 if __name__=="__main__":
